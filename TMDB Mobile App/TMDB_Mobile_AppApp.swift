d5242e02bde688d23fd38ee54a6c89027ae75403
Maximiliano Riquelme Vera <maximiliano.riquelme.-nd@disney.com>
2025-03-17 13:31:00 -0300

    Initial Commit

---
+++ TMDB Mobile App/TMDB_Mobile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_Mobile_AppApp.swift
+//  TMDB Mobile App
+//
+//  Created by Maximiliano Riquelme Vera on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_Mobile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
